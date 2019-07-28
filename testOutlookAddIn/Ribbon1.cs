660cae5c2ce973f42e0c5011b5443e6b9ff71690
Andrew <andrewwos88@gmail.com>
2019-07-28 23:58:52 +0700

    + Add files
    Добавлен Ribbon, панель с кнопками только в режиме просмотра письма, какой-то страшный файл с расширением pfx

---
+++ testOutlookAddIn/Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace testOutlookAddIn
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Hello mf");
+        }
+    }
+}
