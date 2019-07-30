700926015aa6a8a52d8ac69acd4259668c54ca32
Andrew <andrewwos88@gmail.com>
2019-07-30 15:17:15 +0700

    + Сохранение сообщения
    Сохранение открытого сообщения по пути в папку "Мои документы"

---
+++ testOutlookAddIn/FileProcess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testOutlookAddIn
+{
+    static class FileProcess
+    {
+        private static string PathToMainFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        /// <summary>
+        /// Сохранение данных в файл .txt по пути "Мои документы"
+        /// </summary>
+        /// <param name="nameOfFile">Имя файла</param>
+        /// <param name="body">Содержимое файла</param>
+        public static void SaveToFile(string nameOfFile, string body)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(File.Open(PathToMainFile + $@"\{nameOfFile}.txt", FileMode.Create)))
+                {
+                    streamWriter.Write(body);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка записи", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+    }
+}
--- testOutlookAddIn/ThisAddIn.cs
+++ testOutlookAddIn/ThisAddIn.cs
@@ -12,6 +12,7 @@ namespace testOutlookAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
